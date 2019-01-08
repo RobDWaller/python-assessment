a16333ab5e08d3e1a569168a44ada88392d230cc
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2019-01-08 13:16:52 +0000

    Add amend_domain_values and basic test.

--- data_extractor.py
+++ data_extractor.py
@@ -8,14 +8,28 @@ class DataExtractor:
     def __init__(self):
         self.data = WEBSITES
 
-    def find_items(self, value):
+    def find_items(self, value=4):
         """
-        Find and return a new list of items where key "value" is greater than or equal to parameter value.
+        Find and return a new list of items where key "value" is greater than or equal to parameter value. Default = 4.
         :return: list(dict), list of dictionaries matching the above filtering rule.
         """
         return [item for item in self.data if item.get('value') and item.get('value') >= value]
 
+    def amend_domain_values(self, prefix='www.'):
+        """
+        Fixes missing parts of the domain names.
+        :param prefix: str, prefix to add to the domain name. Default = 'www'.
+        :return: amended: list(dict), amended list of web records.
+        """
+        amended = []
+        for item in self.data:
+            if item.get('domain') and not item.get('domain').startswith(prefix):
+                item['domain'] = f"{prefix}{item['domain']}"
+            amended.append(item)
+        return amended
 
-# data_extractor = DataExtractor()
-# print(data_extractor.find_items(4))
-# print(len(data_extractor.find_items(4)))
+
+data_extractor = DataExtractor()
+# print(data_extractor.amend_domain_values())
+print(data_extractor.find_items(4))
+print(len(data_extractor.find_items(4)))
--- tests/test_data_extractor.py
+++ tests/test_data_extractor.py
@@ -29,4 +29,61 @@ class TestDataExtractor:
                 'value': 5
             }
         ]
-        assert data_extractor.find_items(4) == expected
+        assert data_extractor.find_items() == expected
+
+    def test_amend_domain_values(self):
+        expected = [
+            {
+                'name': 'Google',
+                'url': 'https://www.google.co.uk',
+                'domain': 'www.google.co.uk',
+                'secure': True,
+                'value': 5},
+            {
+                'name': 'Facebook',
+                'url': 'https://developers.facebook.com/blog/post/2018/10/02/facebook-login-update/',
+                'domain': 'www.facebook.com',
+                'secure': True, 'value': 4},
+            {
+                'name': 'Bing',
+                'url': 'https://www.bing.com/search?q=athlete&qs=n&form=QBLH&sp=-1&pq=athlete&sc=8-7&sk=&cvid=53830DD7FB2E47B7A5D9CF27F106BC9A',
+                'domain': 'www.bing.com',
+                'secure': False,
+                'value': 3
+            },
+            {
+                'name': 'Ask',
+                'url': 'https://uk.ask.com/web?o=0&l=dir&qo=serpSearchTopBox&q=jupiter',
+                'domain': 'www.ask.com',
+                'secure': False,
+                'value': 1},
+            {
+                'name': 'Duck Duck Go',
+                'url': 'http://duckduckgo.com/?q=plane&t=h_&ia=web',
+                'domain': 'www.duckduckgo.com',
+                'secure': True,
+                'value': 2
+            },
+            {
+                'name': 'Vimeo',
+                'url': 'https://vimeo.com/53812885',
+                'domain': 'www.vimeo.com',
+                'secure': False,
+                'value': 2
+            },
+            {
+                'name': 'YouTube',
+                'url': 'https://www.youtube.com/watch?v=09Cd7NKKvDc',
+                'domain': 'www.youtube.com',
+                'secure': True,
+                'value': 5
+             },
+            {
+                'name': 'Daily Motion',
+                'url': 'http://www.dailymotion.com/search/football',
+                'domain': 'www.dailymotion.com',
+                'secure': True,
+                'value': 1
+            }
+        ]
+        assert data_extractor.amend_domain_values() == expected
